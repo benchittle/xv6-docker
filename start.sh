c604c363266600bbfb318a7f08feb01df5364b82
Ben Chittle <benchittle@me.com>
2023-01-05 19:28:09 -0500

    Check if systemctl command is available first

--- start.sh
+++ start.sh
@@ -1,7 +1,10 @@
 !/bin/bash
-
-if ! systemctl is-active docker.service; then
-    systemctl start docker
+if which systemctl; then
+    if ! systemctl is-active docker.service; then
+        systemctl start docker
+    fi
+else
+    echo "'systemctl' command not found. If you get the error 'Cannot connect to the Docker daemon', you'll have to manually enable it"
 fi
 
 ./xv6-container
