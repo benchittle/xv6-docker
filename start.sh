123d25616290cfd431b0b0f4d8badad6c55d7f93
Ben Chittle <72046603+benchittle@users.noreply.github.com>
2023-01-13 15:07:44 -0500

    Clarify error message

--- start.sh
+++ start.sh
@@ -4,7 +4,7 @@ if which systemctl; then
         systemctl start docker
     fi
 else
-    echo "'systemctl' command not found. If you get the error 'Cannot connect to the Docker daemon', you'll have to manually enable it"
+    echo "'systemctl' command not found. If you get the error 'Cannot connect to the Docker daemon', you'll have to manually enable the daemon"
 fi
 
 ./xv6-container
